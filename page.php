fd10bb5c419a54225bd10c78dc6847d017ab3e72
anandvipul <anandvipul01@gmail.com>
2024-05-21 13:17:13 +0530

    Menu of Child Pages

--- page.php
+++ page.php
@@ -32,16 +32,34 @@ get_header();
 
 ?>
 
-      
-<!-- 
+      <?php 
+      $testArray = get_pages(array(
+        "child_of" => get_the_ID()
+      ));
+
+      if ($theParent or $testArray) {  ?>
+ 
       <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+        <h2 class="page-links__title"><a href="<?php echo get_permalink( $theParent ) ?>"><?php echo get_the_title($theParent);  ?></a></h2>
         <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
+
+        <?php  
+        
+        wp_list_pages(array("title_li" => NULL, 
+      "child_of" => $theParent == 0 ? get_the_ID() : wp_get_post_parent_id(get_the_ID()),
+      "sort_column" => "menu_order"
+      ));
+
+?>
+        <!--  
+        <li class="current_page_item"><a href="#">Our History</a></li>
           <li><a href="#">Our Goals</a></li>
+-->
         </ul>
       </div>
--->
+
+<?php } ?>
+
       <div class="generic-content">
         <?php  the_content(); ?>
       </div>
