6a218347da7da471477fe00da577f0b14340faa2
anandvipul <anandvipul01@gmail.com>
2024-05-27 15:54:33 +0530

    creating-relationships-between-content

--- functions.php
+++ functions.php
@@ -17,7 +17,11 @@ function university_features() {
 }
 
 function university_adjust_queries( $query ) {
-
+	if ( ! is_admin() and is_post_type_archive( "program" ) and is_main_query() ) {
+		$query->set( "orderby", "title" );
+		$query->set( 'order', "ASC" );
+		$query->set( "posts_per_page", -1 );
+	}
 	$today = date( 'Ymd' );
 	if ( ! is_admin() and is_post_type_archive( 'event' ) and $query->is_main_query() ) {
 		$query->set( 'meta_key', "event_date" );
@@ -40,3 +44,4 @@ add_action( "wp_enqueue_scripts", "university_files" );
 add_action( "after_setup_theme", "university_features" );
 
 add_action( "pre_get_posts", "university_adjust_queries" );
+
